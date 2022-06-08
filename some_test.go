10f12e411fc285d5abe19fcb9ad63d9ef0f139be
Jim van Kleef <vankleefjim@gmail.com>
2022-06-09 00:02:49 +0200

    add table-based test with os.Setenv

--- some_test.go
+++ some_test.go
@@ -1,7 +1,21 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestSomething(t *testing.T) {
+	tests := map[string]struct {
+		fooValue string
+	}{
+		"does something": {fooValue: "bar"},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			os.Setenv("foo", tc.fooValue)
+		})
+	}
 
 }
